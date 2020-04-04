e2c57c1d2b6fe4d663f01eb2fd17259ae59e0e5c
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2020-04-04 11:57:34 -0300

    Organiza rotas públicas e protegidas com token

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,23 +7,20 @@ import CheckToken from "../config/auth/CheckToken";
 
 const routes = new Router();
 
-routes.get("/api/usuarios", CheckToken, UsuarioController.findAll);
-routes.post("/api/usuarios", CheckToken, UsuarioController.save);
-routes.get("/api/usuarios/:id", CheckToken, UsuarioController.findById);
-routes.put("/api/usuarios/:id", CheckToken, UsuarioController.update);
-routes.delete("/api/usuarios/:id", CheckToken, UsuarioController.delete);
-routes.get(
-  "/api/usuarios/email/:email",
-  CheckToken,
-  UsuarioController.findByEmail
-);
-
+routes.post("/api/usuarios", UsuarioController.save);
 routes.post("/api/auth/token", AuthController.auth);
 
-routes.get("/api/permissoes", CheckToken, PermissaoController.findAll);
+routes.use(CheckToken);
+
+routes.get("/api/usuarios", UsuarioController.findAll);
+routes.get("/api/usuarios/:id", UsuarioController.findById);
+routes.put("/api/usuarios/:id", UsuarioController.update);
+routes.delete("/api/usuarios/:id", UsuarioController.delete);
+routes.get("/api/usuarios/email/:email", UsuarioController.findByEmail);
+
+routes.get("/api/permissoes", PermissaoController.findAll);
 routes.get(
   "/api/permissoes-usuario",
-  CheckToken,
   PermissaoController.findAllPermissoesUsuario
 );
 
